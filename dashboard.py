511ea7fd3f72f8ef69c5e2f7793504ed0379eb45
Andi Muhammad Hakam <113480196+ANDIMUHAMMADHAKAM@users.noreply.github.com>
2024-08-18 08:23:58 +0800

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import mathplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import warnings
 from datetime import datetime
 import plotly.express as px
